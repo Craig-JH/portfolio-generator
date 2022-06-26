e15550fa6c321e393c2c44b135095fac8f900ce0
Craig Hyman <misccorp@yahoo.com>
2022-06-26 13:11:26 -0400

    Print dynamic output

--- app.js
+++ app.js
@@ -2,20 +2,17 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-  };
     
+    for (let i=0; i < profileDataArr.length; i+=1) {
+        console.log(profileDataArr[i]);
 
+    }
+
+    console.log('======================');
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+    
+};
 
 printProfileData(profileDataArgs);
 
